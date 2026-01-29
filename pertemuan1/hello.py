f5ec374257107cd08b1e334ea0c3b2223b449df5
chikazhra23 <chkazahra@gmail.com>
2026-01-29 19:48:51 +0700

    Tambah tugas praktikum

---
+++ pertemuan1/hello.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+print(1+2)
+print('Hello World!')
+ 
+ #contoh komentar di python
+print("Hello World!")
+print("Komentar")
+
+#contoh variabel
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#contoh casting 
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#contoh type
+print(type(y))
+print(type(x))
+print(type(z))
+
